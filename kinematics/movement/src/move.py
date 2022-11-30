0a43b899677ef41ae60497e1d1ee013a2ccbd720
Class Account <ee106a-agw@c105-1.local>
2022-11-29 16:45:16 -0800

    PID

--- kinematics/movement/src/move.py
+++ kinematics/movement/src/move.py
@@ -7,48 +7,68 @@ import nav_msgs
 from nav_msgs.msg import Odometry
 
 def talker():
-    global diffX
-    global diffY
+    global curX
+    global curY
     pub = rospy.Publisher('/cmd_vel', Twist, queue_size=10)
     sub = rospy.Subscriber('/odom', Odometry, callback)
     rospy.init_node('talker', anonymous=True)
     rate = rospy.Rate(10) # 10hz
-    K1 = 0.3
+    K1 = 0.2
     K2 = 1
+    KP = 0.3
+    KD = 0.02
+    dt = 0.01
+    goalX = 2.5
+    goalY = 0.4
+    prevX = 0
+    prevY = 0
+    sumX = 0
+    sumY = 0
     while not rospy.is_shutdown():
         #PID control equations here
         control_command = Twist()
-        print(diffX, diffY)
-        control_command.linear.x = K1 * diffX#constant value when z=0, 0 when z!=0
-        control_command.linear.y = 0
-        control_command.linear.z = 0
-        control_command.angular.x = 0
-        control_command.angular.y = 0
-        control_command.angular.z = K2 * diffY#odom and position of next point
 
-        pub.publish(control_command)
+
+        errX = curX - goalX
+        errY = curY - goalY
+        print(errX, errY)
+        while (abs(errX) > 0.01 and abs(errY) > 0.01):
+            sumX += errX * dt
+            dedtX = (errX - prevX) / dt
+            wX = KP*errX + K1 * sumX + KD * dedtX
+            prevX = errX
+
+            
+            sumY += errY * dt
+            dedtY = (errY - prevY) / dt
+            wY = KP*errY + K1 * sumY + KD * dedtY
+            prevY = errY
+
+
+        
+
+            #if (diffX < 0.01, and diffY < 0.01):
+
+            control_command.linear.x = wX
+            control_command.linear.y = 0
+            control_command.linear.z = 0
+            control_command.angular.x = 0
+            control_command.angular.y = 0
+            control_command.angular.z = -1 * wY
+
+            pub.publish(control_command)
         rate.sleep()
 
 def callback(data):
-    global goalX
-    global goalY
-    global diffX
-    global diffY
-    global goalChanged
-    if not goalChanged:
-        goalX = data.pose.pose.position.x + 0.5
-        goalY = data.pose.pose.position.y + 0.5
-        goalChanged = True
-    diffX = goalX - data.pose.pose.position.x
-    diffY = goalY - data.pose.pose.position.y
+    global curX
+    global curY
+    
+    curX = data.pose.pose.position.x
+    curY = data.pose.pose.position.y
+    print(f"current x = {curX}, current y = {curY}")
 
 if __name__ == '__main__':
     try:
-        goalX = 0
-        goalY = 0
-        diffX = 0
-        diffY = 0
-        goalChanged = False
         talker()
     except rospy.ROSInterruptException:
         pass
