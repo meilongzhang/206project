9de90159215a3ded3b5c8f6c4c2075f6835c90bb
Class Account <ee106a-adf@c105-2.local>
2022-11-29 16:49:31 -0800

    adding obstacle deetction files

---
+++ obstacle/build/catkin_generated/setup_cached.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env sh
+# generated from catkin/python/catkin/environment_cache.py
+
+# based on a snapshot of the environment before and after calling the setup script
+# it emulates the modifications of the setup script without recurring computations
+
+# new environment variables
+
+# modified environment variables
+export CMAKE_PREFIX_PATH="/home/cc/ee106a/fa22/class/ee106a-adf/ros_workspaces/206project/obstacle/devel:$CMAKE_PREFIX_PATH"
+export LD_LIBRARY_PATH="/home/cc/ee106a/fa22/class/ee106a-adf/ros_workspaces/206project/obstacle/devel/lib:$LD_LIBRARY_PATH"
+export PWD='/home/cc/ee106a/fa22/class/ee106a-adf/ros_workspaces/206project/obstacle/build'
+export ROSLISP_PACKAGE_DIRECTORIES='/home/cc/ee106a/fa22/class/ee106a-adf/ros_workspaces/206project/obstacle/devel/share/common-lisp'
+export ROS_PACKAGE_PATH="/home/cc/ee106a/fa22/class/ee106a-adf/ros_workspaces/206project/obstacle/src:$ROS_PACKAGE_PATH"
