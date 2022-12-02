6f5b88ff7ddba25aade5b1d0cc20496d3bf25539
Steed Amegbor <a.steed@yahoo.fr>
2022-12-02 07:26:57 -0800

    improved PID controller

--- kinematics/movement/src/move.py
+++ kinematics/movement/src/move.py
@@ -3,6 +3,7 @@
 import rospy
 from std_msgs.msg import String
 from geometry_msgs.msg import Twist, Point 
+from tf.transformations import euler_from_quaternion
 import tf2_ros
 import nav_msgs
 from nav_msgs.msg import Odometry
@@ -15,13 +16,15 @@ def talker():
     # sub = rospy.Subscriber('/odom', Odometry, callback)
     #get  tf to enable getting the robot pose 
     tfBuffer = tf2_ros.Buffer()
-    # tfListener = tf2_ros.TransformListener(tfBuffer)
+    tfListener = tf2_ros.TransformListener(tfBuffer)
     rospy.init_node('talker', anonymous=True)
     rate = rospy.Rate(10) # 10hz
-    K1 = 0.2
-    K2 = 1
+    KI = 0.2
     KP = 0.3
     KD = 0.02
+    KP_a = 0.3
+    KI_a = 0.2
+    KD_a = 0.02
     dt = 0.01
     goalX = 2.5
     goalY = 0.4
@@ -34,12 +37,14 @@ def talker():
     prev_err_t = 0
     sum_err_r = 0 
     prev_err_r = 0
-    trans, rot = tfBuffer.lookup_transform('base_link','odom', rospy.Time())
-    curX = trans.tranform.translation.x
-    curY = trans.tranform.translation.y
+    trans, rot = tfListener.lookup_transform('base_footprint','odom', rospy.Time(0))
+    rot = euler_from_quaternion(rot)
+    trans = Point(*trans)
+    curX = trans.x
+    curY = trans.y
+    control_command = Twist()
     while not rospy.is_shutdown():
         #PID control equations here
-        control_command = Twist()
         # errX = curX - goalX
         # errY = curY - goalY
         # print(errX, errY)
@@ -71,12 +76,43 @@ def talker():
         # rate.sleep()
 
         #calulate the translation error 
+                #translation
+        err_t = mt.sqrt((goalX - curX)**2 + (goalY - curY)**2)
+        angl_t = mt.atan2((goalY - curY, goalX - curX))
+        prev_angle = 0 
+        total_angle = 0
+        while err_t > 0.05:
+            trans, rot = tfListener.lookup_transform('base_footprint','odom', rospy.Time(0))
+            rot = euler_from_quaternion(rot)
+            trans = Point(*trans)
+            curX = trans.x
+            curY = trans.y
+            #PID for err_t 
+            sum_err_t += err_t * dt
+            dedt_err_t = (err_t - prev_err_t) / dt
+            wX = KP*err_t + KI * sum_err_t + KD * dedt_err_t
+            prev_err_t = err_t
+
+            #PID for err_angl
+            diff_angle = (angl_t - prev_angle)/dt
+            total_angle += angl_t * dt
+            Wz = KP_a * angl_t + KI_a * total_angle + KD_a * diff_angle
+            prev_angle = angl_t
+            control_command.linear.x = 0
+            control_command.linear.y = wX
+            control_command.linear.z = 0
+            control_command.angular.x = 0
+            control_command.angular.y = 0
+            control_command.angular.z = Wz
+
+            pub.publish(control_command)
+        rate.sleep()
         err_r = abs(goalZ - rot[2]) 
         while err_r > 0.1:
             #PID for err_t 
             sum_err_r += err_r * dt
             dedt_err_r = (err_r - prev_err_r) / dt
-            wZ = KP*err_r + K1 * sum_err_r + KD * dedt_err_r
+            wZ = KP*err_r + KI * sum_err_r + KD * dedt_err_r
             prev_err_t = err_t
 
             #if (diffX < 0.01, and diffY < 0.01):
@@ -90,25 +126,7 @@ def talker():
 
             pub.publish(control_command)
         rate.sleep()
-        err_t = mt.sqrt((goalX - curX)**2 + (goalY - curY)**2)
-        while err_t > 0.05:
-            #PID for err_t 
-            sum_err_t += err_t * dt
-            dedt_err_t = (err_t - prev_err_t) / dt
-            wX = KP*err_t + K1 * sum_err_t + KD * dedt_err_t
-            prev_err_t = err_t
 
-            #if (diffX < 0.01, and diffY < 0.01):
-
-            control_command.linear.x = wX
-            control_command.linear.y = 0
-            control_command.linear.z = 0
-            control_command.angular.x = 0
-            control_command.angular.y = 0
-            control_command.angular.z = 0
-
-            pub.publish(control_command)
-        rate.sleep()
 
 # def callback(data):
 #     global curX
